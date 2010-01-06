e3634b64f389f4813855964ebd603b88f5d006c4
Matthew Landauer <matthew@openaustralia.org>
2010-01-07 09:21:32 +1100

    Added Park class to data model

---
+++ Classes/Park.h
@@ -0,0 +1,15 @@
+#import <CoreData/CoreData.h>
+
+
+@interface Park :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSNumber * latitude;
+
+@end
+
+
+
---
+++ Classes/Park.m
@@ -0,0 +1,10 @@
+#import "Park.h"
+
+
+@implementation Park 
+
+@dynamic name;
+@dynamic longitude;
+@dynamic latitude;
+
+@end
