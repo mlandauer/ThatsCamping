d9813d7e37a2406a2911e8936d5a8f1b2fee5315
Matthew Landauer <matthew@openaustralia.org>
2010-01-08 17:05:54 +1100

    Separated code for getting more details campsite info into a separate script

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# TODO: There's a problem here "Bombah Broadwater: campgrounds on the eastern    shore"
+
 require 'rubygems'
 require 'mechanize'
 require 'simple_struct'
@@ -50,9 +52,6 @@ ActiveRecord::Schema.define do
     t.column :caravans, :boolean
     t.column :trailers, :boolean
     t.column :car, :boolean
-    t.column :road_access, :text
-    t.column :fees, :text
-    t.column :no_sites, :integer
   end
 end
 
@@ -165,54 +164,4 @@ page.search('#SearchResults')[1].search('tr')[1..-1].each do |camp|
     end
   end
   c.save!
-end
-
-Park.find(:all).each do |park|
-  puts "Processing page #{park.campsites_url}..."
-  page = agent.get(park.campsites_url)
-
-  a = page.at('div > a[@name]')
-
-  results = {}
-  while a
-    content = a.next
-    a2 = content.search('a[@name]').find{|t| t.attributes['name'].to_s[0..0] == 'c'}
-    if a2
-      content2 = a2.next
-      content.add_next_sibling(a2)
-      a2.add_next_sibling(content2)
-      results[a.attributes['name'].to_s] = content
-    end
-    a = a2
-  end
-
-  results.each do |web_id, result|
-    site = Campsite.find(:first, :conditions => {:web_id => web_id})
-    if site.nil?
-      puts "WARNING: Strange. Can't find campsite with web_id: #{web_id}. So, skipping"
-    else
-      road_access_heading = result.at('#relatedLinks').search('.heading').find{|h| h.inner_text == "Road access"}
-      site.road_access = road_access_heading.next.inner_text.strip if road_access_heading
-      fees = result.at('#relatedLinks').search('.heading').find{|h| h.inner_text == "Fees"}
-      if fees
-        site.fees = ""
-        current = fees.next
-        while current
-          site.fees += current.to_s
-          current = current.next
-        end
-      end
-      if result.at('h3').inner_text.strip =~ /\((\d+) sites\)/
-        site.no_sites = $~[1].to_i
-      end
-      site.save!
-    end
-  end
-end
-
-Park.find(:all, :order => :name).each do |park|
-  puts "#{park.name}:"
-  park.campsites(:order => :name).each do |s|
-    puts "  #{s.name}, No sites: #{s.no_sites}, Facilities: #{s.toilets}, #{s.picnic_tables}, #{s.barbecues}, #{s.showers}, #{s.drinking_water}, Length walk: #{s.length_walk}, Caravans: #{s.caravans}, Trailers: #{s.trailers}, Car: #{s.car}"
-  end
 end
---
+++ scraper/scraper_campsite_details.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+
+# Scrapes more details about campsites. This can only be run after "scraper.rb"
+
+require 'rubygems'
+require 'mechanize'
+require 'db'
+require 'park'
+require 'campsite'
+
+# Create the database columns we need for this script to work
+# Need to comment these out if the columns have already been created. Need a better solution
+ActiveRecord::Schema.define do
+  add_column :campsites, :road_access, :text
+  add_column :campsites, :fees, :text
+  add_column :campsites, :no_sites, :integer
+end
+
+agent = WWW::Mechanize.new
+
+Park.find(:all).each do |park|
+  puts "Processing page #{park.campsites_url}..."
+  page = agent.get(park.campsites_url)
+
+  a = page.at('div > a[@name]')
+
+  results = {}
+  while a
+    content = a.next
+    a2 = content.search('a[@name]').find{|t| t.attributes['name'].to_s[0..0] == 'c'}
+    if a2
+      content2 = a2.next
+      content.add_next_sibling(a2)
+      a2.add_next_sibling(content2)
+      results[a.attributes['name'].to_s] = content
+    end
+    a = a2
+  end
+
+  results.each do |web_id, result|
+    site = Campsite.find(:first, :conditions => {:web_id => web_id})
+    if site.nil?
+      puts "WARNING: Strange. Can't find campsite with web_id: #{web_id}. So, skipping"
+    else
+      road_access_heading = result.at('#relatedLinks').search('.heading').find{|h| h.inner_text == "Road access"}
+      site.road_access = road_access_heading.next.inner_text.strip if road_access_heading
+      fees = result.at('#relatedLinks').search('.heading').find{|h| h.inner_text == "Fees"}
+      if fees
+        site.fees = ""
+        current = fees.next
+        while current
+          site.fees += current.to_s
+          current = current.next
+        end
+      end
+      if result.at('h3').inner_text.strip =~ /\((\d+) sites\)/
+        site.no_sites = $~[1].to_i
+      end
+      site.save!
+    end
+  end
+end
+
+Park.find(:all, :order => :name).each do |park|
+  puts "#{park.name}:"
+  park.campsites(:order => :name).each do |s|
+    puts "  #{s.name}, No sites: #{s.no_sites}, Facilities: #{s.toilets}, #{s.picnic_tables}, #{s.barbecues}, #{s.showers}, #{s.drinking_water}, Length walk: #{s.length_walk}, Caravans: #{s.caravans}, Trailers: #{s.trailers}, Car: #{s.car}"
+  end
+end
