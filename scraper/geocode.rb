c3c30d252504e7419a41c81259866c9783c389ba
Matthew Landauer <matthew@openaustralia.org>
2010-01-09 12:23:39 +1100

    Some random changes to the play geocoder

--- scraper/geocode.rb
+++ scraper/geocode.rb
@@ -12,10 +12,7 @@ require 'db'
 
 # Turn a name like "Smith Campground" into "Smith"
 def remove_name_ending(name)
-  #special_phrases = ["campground and picnic area", "large group campground", "picnic and camping area",
-  #  "camping and picnic area", "campground", "campgrounds", "camping area", "camping ground", "camping grounds", "camp",
-  #  "rest area", "tourist park"]
-  special_phrases = ["picnic and camping area", "campground and picnic area", "camping and picnic area",
+  special_phrases = ["picnic and camping area", "campground and picnic area", "camping and picnic area", "large group campground",
     "camping ground", "campground", "camping area", "camp", "rest area", "tourist park", "campgrounds", "camping grounds"]
   shorter = name
   special_phrases.each do |phrase|
@@ -24,22 +21,36 @@ def remove_name_ending(name)
   shorter
 end
 
+#campsites = Campsite.find(:all)
+# First find any campsites that we can make exact matches for
+#campsites.each do |campsite|
+#  puts "Checking #{campsite.name}"
+#  matches = Location.find(:all, :conditions => ["name LIKE ?", campsite.name])
+#  if matches.size == 1
+#    puts "Perfect match for: #{campsite.name}"
+#    #campsite.delete
+#    perfect_match_count += 1
+#  elsif matches.size > 1
+#    match_names = matches.map{|m| m.name}
+#    puts "Multiple matches for #{campsite.name}: #{match_names.join(" or ")}"
+#  end
+#end
+#exit
+  
 multiple_match_count, no_match_count, match_count = 0, 0, 0
 Campsite.find(:all).each do |campsite|
   match_text = remove_name_ending(campsite.name)
-  puts "#{campsite.name} -> #{match_text}"
-  #puts match_text
-  #matches = PoiLocation.find(:all, :conditions => ["name LIKE ?", "%#{match_text}%"])
-  #if matches.size > 1
-  #  puts "Multiple matches for: #{campsite.name}"
-  #  multiple_match_count += 1
-  #elsif matches.size == 0
-  #  puts "No match for: #{campsite.name}"
-  #  no_match_count += 1
-  #else
-  #  puts "Unique match: #{campsite.name} matches #{matches.first.name}"
-  #  match_count += 1
-  #end
+  matches = Location.find(:all, :conditions => ["name LIKE ?", "%#{match_text}%"])
+  if matches.size > 1
+    puts "Multiple matches for: #{campsite.name}"
+    multiple_match_count += 1
+  elsif matches.size == 0
+    puts "No match for: #{campsite.name}"
+    no_match_count += 1
+  else
+    puts "Unique match: #{campsite.name} matches #{matches.first.name}"
+    match_count += 1
+  end
 end
 
 puts "\nStatistics on matches: multiple: #{multiple_match_count}, no: #{no_match_count}, match: #{match_count}"
