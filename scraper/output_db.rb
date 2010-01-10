c4773d6c9a8f3887149088e1017891f72ade6bf1
Matthew Landauer <matthew@openaustralia.org>
2010-01-10 13:40:18 +1100

    Shorten names of campsite and parks

--- scraper/output_db.rb
+++ scraper/output_db.rb
@@ -9,6 +9,18 @@ require 'builder'
 require 'db'
 require 'park'
 require 'campsite'
+require 'utils'
+
+def shorten_park_name(name)
+  remove_phrases_at_end(name,
+    ["National Park", "State Conservation Area", "Nature Reserve", "Conservation Reserve", "Historic Site"])
+end
+
+def shorten_campsite_name(name)
+  remove_phrases_at_end(name,
+    ["campground and picnic area", "picnic and camping area", "camping and picnic area", "large group campground",
+    "campground", "camping area", "camping ground", "campgrounds", "tourist park", "camping grounds", "rest area"])
+end
 
 File.open("#{File.dirname(__FILE__)}/../Parks.plist", "w") do |f|
   x = Builder::XmlMarkup.new(:target => f, :indent => 2)
@@ -18,7 +30,7 @@ File.open("#{File.dirname(__FILE__)}/../Parks.plist", "w") do |f|
     x.array {
       Park.find(:all).each do |park|
         x.dict {
-          x.key "name";   x.string park.name
+          x.key "name";   x.string shorten_park_name(park.name)
           x.key "webId";  x.string park.web_id
         }        
       end
@@ -34,7 +46,7 @@ File.open("#{File.dirname(__FILE__)}/../Campsites.plist", "w") do |f|
     x.array {
       Campsite.find(:all).each do |campsite|
         x.dict {
-          x.key "name"; x.string campsite.name
+          x.key "name"; x.string shorten_campsite_name(campsite.name)
           if campsite.latitude && campsite.longitude
             x.key "latitude"; x.real campsite.latitude
             x.key "longitude"; x.real campsite.longitude
