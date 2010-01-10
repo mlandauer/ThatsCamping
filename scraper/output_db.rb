9bbe173f574cf841d70b96cc91b5161fdf28fc25
Matthew Landauer <matthew@openaustralia.org>
2010-01-10 13:24:43 +1100

    Now takes the real database data

--- scraper/output_db.rb
+++ scraper/output_db.rb
@@ -2,8 +2,13 @@
 
 # Output the contents of the database to a property list than can be used by the application
 
+$:.unshift "#{File.dirname(__FILE__)}/lib"
+
 require 'rubygems'
 require 'builder'
+require 'db'
+require 'park'
+require 'campsite'
 
 File.open("#{File.dirname(__FILE__)}/../Parks.plist", "w") do |f|
   x = Builder::XmlMarkup.new(:target => f, :indent => 2)
@@ -11,18 +16,12 @@ File.open("#{File.dirname(__FILE__)}/../Parks.plist", "w") do |f|
   x.declare! :DOCTYPE, :plist, :PUBLIC, "-//Apple//DTD PLIST 1.0//EN", "http://www.apple.com/DTDs/PropertyList-1.0.dtd"
   x.plist(:version => "1.0") {
     x.array {
-      x.dict {
-        x.key "name"
-        x.string "Blue Mountains"
-        x.key "webId"
-        x.string "BLUE"
-      }
-      x.dict {
-        x.key "name"
-        x.string "Kanangra-Boyd"
-        x.key "webId"
-        x.string "KAN"
-      }    
+      Park.find(:all).each do |park|
+        x.dict {
+          x.key "name";   x.string park.name
+          x.key "webId";  x.string park.web_id
+        }        
+      end
     }
   }
 end
@@ -33,39 +32,17 @@ File.open("#{File.dirname(__FILE__)}/../Campsites.plist", "w") do |f|
   x.declare! :DOCTYPE, :plist, :PUBLIC, "-//Apple//DTD PLIST 1.0//EN", "http://www.apple.com/DTDs/PropertyList-1.0.dtd"
   x.plist(:version => "1.0") {
     x.array {
-      x.dict {
-        x.key "name";       x.string  "Perrys Lookdown"
-        x.key "latitude";   x.real    -33.598333
-    		x.key "longitude";  x.real    150.351111
-    		x.key "webId";      x.string  "foo"
-    		x.key "parkWebId";  x.string  "BLUE"
-      }
-      x.dict {
-    		x.key "name";       x.string  "Euroka Clearing"
-    		x.key "latitude";   x.real    -33.798333
-    		x.key "longitude";  x.real    150.617778
-    		x.key "webId";      x.string  "foo"
-    		x.key "parkWebId";  x.string  "BLUE"
-    	}
-    	x.dict {
-    		x.key "name";       x.string  "Murphys Glen"
-    		x.key "latitude";   x.real    -33.765
-    		x.key "longitude";  x.real    150.501111
-    		x.key "webId";      x.string  "foo"
-    		x.key "parkWebId";  x.string  "BLUE"
-    	}
-    	x.dict {
-    		x.key "name";       x.string  "Dingo Dell"
-    		x.key "latitude";   x.real    -33.97375
-    		x.key "longitude";  x.real    149.96516
-    		x.key "webId";      x.string  "foo"
-    		x.key "parkWebId";  x.string  "KAN"
-    	}
-    	x.dict {
-    		x.key "name";       x.string  "Boyd River"
-    		x.key "webId";      x.string  "foo"
-    		x.key "parkWebId";  x.string  "KAN"
-    	}
+      Campsite.find(:all).each do |campsite|
+        x.dict {
+          x.key "name"; x.string campsite.name
+          if campsite.latitude && campsite.longitude
+            x.key "latitude"; x.real campsite.latitude
+            x.key "longitude"; x.real campsite.longitude
+          end
+          x.key "webId"; x.string campsite.web_id
+          x.key "parkWebId"; x.string campsite.park.web_id
+        }
+      end
     }
   }
 end
