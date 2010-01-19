57fb2f5cb4863637e77c182ee2fc7aba46aae861
Matthew Landauer <matthew@openaustralia.org>
2010-01-19 16:38:13 +1100

    Extracted function for turning html into some plain text

--- scraper/scraper_campsite_details.rb
+++ scraper/scraper_campsite_details.rb
@@ -63,43 +63,7 @@ Park.find(:all).each do |park|
       puts "*** #{site.name} ***"
       content.at('div#relatedLinks').remove
       content.at('h3').remove
-      description = Nokogiri::HTML.fragment("<div>" + content.children.find_consecutive{|p| p.at('strong').nil? }.join + "</div>")
-      # Remove images and links associated with them
-      description.search('a > img').each{|i| i.parent.remove}
-      description.search('img').remove
-      description.search('div#footer').remove
-      description.search('div.clearRight').remove
-      description.search('script').remove
-      # Turn occurences of "<a>foo</a>" into "foo"
-      description.search('a').each {|a| replace_with_inside(a)}
-      description.search('b').each {|a| replace_with_inside(a)}
-      description.search('font').each {|a| replace_with_inside(a)}
-      description.search('span').each {|a| replace_with_inside(a)}
-      description.search('ul').each {|a| replace_with_inside(a)}
-      description.search('li').each {|a| replace_with_inside(a)}
-      description.search('div > div').each {|a| replace_with_inside(a)}
-      
-      # Turn all occurences of "<p>Some Text</p>" into "Some Text<br>"
-      description.search('p').each do |p|
-        p.children.each do |c|
-          c.parent = p.parent
-        end
-        br = Nokogiri::XML::Node.new "br", description
-        br.parent = p.parent
-        p.remove
-      end
-      description = description.at('div').inner_html
-      # replace &nbsp; with a simple space
-      description.gsub!("&nbsp;", " ")
-      # Now replace "<br>" by "\n\n"
-      description = simplify_whitespace(description)
-      description.gsub!("<br>", "\n")
-      description.squeeze!("\n")
-      description.gsub!("\n", "\n\n")
-      description = description.strip
-      # Remove comments
-      description.gsub!(/<!--.*-->/, "")
-      # Check that there aren't any tags left over
+      description = html_into_plain_text("<div>" + content.children.find_consecutive{|p| p.at('strong').nil? }.join + "</div>")
       puts description
       raise "Found tag" if description =~ /<.*>/
       site.description = description
